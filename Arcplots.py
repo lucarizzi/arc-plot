ec4bb9b7f437b53a870be77203a2d8615a578975
Luca Rizzi <lrizzi@keck.hawaii.edu>
2017-07-18 16:31:58 -1000

    First working version

--- Arcplots.py
+++ Arcplots.py
@@ -8,21 +8,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
 
-import os
+import os,sys
+
+from bokeh.plotting import figure, output_file, show, curdoc
+from bokeh.models import Range1d, HoverTool, ColumnDataSource, CustomJS
+
+from bokeh.layouts import widgetbox, layout
+from bokeh.models.widgets import Button, RadioButtonGroup, Select, Slider, Dropdown, TextInput
 
 
 # In[5]:
-
+d = dict(one='1', two='2')
 lampList = {
-    "lamp1" : {
-        "file" : "lamp1.dat",
-        "name" : "He"
-    },
-    "lamp2" : {
-        "file" : "lamp2.dat",
-        "name" : "Ar"
-    }
-}
+    'lamp1': dict(file="lamp1.dat", name="He"), 
+    'lamp2': dict(file="lamp2.dat", name="Ar")}
+
 
 directory = "arcplots"
 
@@ -31,33 +31,48 @@ sides = ["red","blue"]
 gratings = {
     "150/7500": {
         "dispersion": 3,
-        "range":12288
+        "range":12288,
+        "id":"150/7500"
     },
-    "300/500": {
+    "300/5000": {
         "dispersion": 1.59,
-        "range":6525
+        "range":6525,
+        "id":"300/5000"
     },
     "400/8500": {
         "dispersion": 1.16,
-        "range": 4762
+        "range": 4762,
+        "id":"400/8500"
     },
     "600/7500": {
         "dispersion": 0.80,
-        "range": 3275
+        "range": 3275,
+        "id":"600/7500"
     },
     "600/5000": {
         "dispersion": 0.80,
-        "range": 3275
+        "range": 3275,
+        "id":"600/5000"
     },
-    "600/1000": {
+    "600/10000": {
         "dispersion": 0.80,
-        "range":3275
+        "range":3275,
+        "id":"600/10000"
     },
     "831/8200": {
         "dispersion": 0.58,
-        "range": 2375
+        "range": 2375,
+        "id":"831/8200"
     }
 }
+
+# convert into a pandas data source
+
+gratings = pd.Series(gratings)
+
+# and make it an object that can be shared with the JavaScript layer
+
+gratings_source = ColumnDataSource(data={'gratings':gratings})
 
 
 for key in lampList.keys():
@@ -71,18 +86,12 @@ for key in lampList.keys():
 
 # In[17]:
 
-from bokeh.plotting import figure, output_file, show, curdoc
-from bokeh.models import Range1d, HoverTool
-
-from bokeh.layouts import widgetbox, layout
-from bokeh.models.widgets import Button, RadioButtonGroup, Select, Slider
-
 
 
 
 # In[6]:
 
-output_file("line.html")
+output_file('arcplot.html')
 hover = HoverTool(tooltips=[
     ("lambda", "$x{00000.0}"),
 ])
@@ -93,21 +102,31 @@ p.add_tools(hover)
 
 # In[7]:
 
-x1=lampList['lamp1']['lines']['lambda']
-y1=gaussian_filter(lampList['lamp1']['lines']['intensity'],sigma=4)
-x2=lampList['lamp2']['lines']['lambda']
-y2=gaussian_filter(lampList['lamp2']['lines']['intensity'],sigma=4)
+source1 = ColumnDataSource(data={
+    "x":lampList['lamp1']['lines']['lambda'],
+    "y":gaussian_filter(lampList['lamp1']['lines']['intensity'],sigma=4)}, 
+    id=lampList['lamp1']['name']
+    )
+source2 = ColumnDataSource(data={
+    "x":lampList['lamp2']['lines']['lambda'],
+    "y":gaussian_filter(lampList['lamp2']['lines']['intensity'],sigma=4)}, 
+    id=lampList['lamp2']['name']
+    )
 
 
 # In[8]:
 
-p.line(x1,y1, color='firebrick', legend=lampList['lamp1']['name'])
-p.line(x2,y2,color='navy', legend=lampList['lamp2']['name'])
-central_wavelength=7500
-range = gratings["600/1000"]["range"]
-xlim_low = central_wavelength-range/2
-xlim_high = central_wavelength+range/2
-p.x_range=Range1d(xlim_low, xlim_high)
+line1 = p.line("x","y", source=source1, color='firebrick', legend=lampList['lamp1']['name'])
+line2 = p.line("x","y", source=source2, color='navy', legend=lampList['lamp2']['name'])
+
+
+initial_grating_selection='600/10000'
+initial_lambda_selection = 7500
+initial_range = gratings[initial_grating_selection]['range']
+initial_xmin = initial_lambda_selection-initial_range/2
+initial_xmax = initial_lambda_selection+initial_range/2
+
+p.x_range=Range1d(initial_xmin, initial_xmax)
 
 
 # In[9]:
@@ -135,29 +154,92 @@ p.yaxis.axis_label = "Intensity"
 p.legend.click_policy="hide"
 
 
+rangeBox = TextInput(title="Wavelength range", value=str(initial_range))
+xminBox = TextInput(title="Minimum wavelength", value=str(initial_xmin))
+xmaxBox = TextInput(title="Maximum Wavelength", value=str(initial_xmax))
+
+select = Select(title="Gratings:",  value=initial_grating_selection, options=[str(x) for x in gratings.keys()])
+
+update_xrange_slider = CustomJS(args=dict(x_range=p.x_range,  gratings=gratings_source, select=select, 
+                                          rangeBox=rangeBox, xminBox= xminBox, xmaxBox= xmaxBox), code="""
+var central = cb_obj.get("value");
+var selected_grating = select.value;
+var gratings_data = gratings.get('data')
+var gratings = gratings_data['gratings']
+var lookup = {};
+for (var i=0, len = gratings.length; i < len; i++) {
+    lookup[gratings[i].id] = gratings[i];
+    }
+var range = lookup[selected_grating.toString()].range;
+var xmin = central-range/2;
+var xmax = central+range/2;
+
+x_range.set("start", xmin);
+x_range.set("end", xmax);
+rangeBox.set("value", range.toString());
+xminBox.set("value", xmin.toString());
+xmaxBox.set("value", xmax.toString());
+""")
+
+slider = Slider(start=3700,end=10000,value=7500, step=1, title="Central wavelength", callback=update_xrange_slider)
+
+update_xrange_select = CustomJS(args=dict(x_range=p.x_range,  gratings=gratings_source, slider=slider,
+                                          rangeBox=rangeBox, xminBox= xminBox, xmaxBox= xmaxBox), code="""
+var central = slider.value;
+var selected_grating = cb_obj.get("value");
+var gratings_data = gratings.get('data')
+var gratings = gratings_data['gratings']
+var lookup = {};
+for (var i=0, len = gratings.length; i < len; i++) {
+    lookup[gratings[i].id] = gratings[i];
+    }
+var range = lookup[selected_grating.toString()].range;
+var xmin = central-range/2;
+var xmax = central+range/2;
+
+x_range.set("start", xmin);
+x_range.set("end", xmax);
+rangeBox.set("value", range.toString());
+xminBox.set("value", xmin.toString());
+xmaxBox.set("value", xmax.toString());
+""")
+
+
+
+
+
+select.js_on_change("change", update_xrange_select)
+
 # In[15]:
+#def update_range(attr,old,new):
+#    grating = str(select.value)
+#    sys.stdout.write(grating)
+#    range = float(gratings[grating]['range'])
+#    xmin = float(slider.value-range/2)
+#    xmax = float(slider.value+range/2)
+#    rangeBox.value = str(range)
+#    xminBox.value = str(xmin)
+#    xmaxBox.value = str(xmax)
+#    p.title.text=select.value
 
-#slider = Slider(start=0, end=10, value=1, step=.1, title="Slider")
-#button_group = RadioButtonGroup(labels=["Option 1", "Option 2", "Option 3"], active=0)
-#select = Select(title="Option:", value="foo", options=["foo", "bar", "baz", "quux"])
-#button_1 = Button(label="Button 1")
-#button_2 = Button(label="Button 2")
 
-show(p)
+#select.on_change('value',update_range)
 
 
 # In[18]:
 
-#l = layout([
-#  [button_group],
-#  [select],
-#  [p],
-#], sizing_mode='stretch_both')
-
+l = layout([
+    [select],
+    [slider],
+    [p],
+    [rangeBox],
+    [xminBox,xmaxBox]
+])
+show(l)
 
 # In[20]:
 
-curdoc().add_root(p)
+curdoc().add_root(l)
 
 
 # In[ ]:
